b370744731fe6f2b3f75bcfe7a62e46c55836c82
bayu-firdaus <firdausbay28@gmail.com>
2022-03-16 20:23:50 +0700

    Bayu Firdaus

---
+++ Laravel-Perpus/resources/views/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layout.main')
+
+@section('title', 'Perpustakaan Mondoluko')
+
+@section('content')
+    <div class="container">
+        <div class="jumbotron">
+            <h1 class="display-4">Perpustakaan Mondoluko</h1>
+            <hr class="my-4">            
+        </div>
+    </div>
+@endsection
